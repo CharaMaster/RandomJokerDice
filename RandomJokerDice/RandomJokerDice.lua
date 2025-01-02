beeb50b14eed07652f68208eef20b987955a5e3e
AmazinDooD <153538539+AmazinDooD@users.noreply.github.com>
2025-01-02 18:09:17 +0000

    add credits :)

--- RandomJokerDice/RandomJokerDice.lua
+++ RandomJokerDice/RandomJokerDice.lua
@@ -32,13 +32,14 @@ local function is_end_of_round(context)
 end
 
 SMODS.Joker {
-  key = 'rage dice',
+  key = 'ragedice',
   loc_txt = {
     name = 'Rage Dice',
     text = {
-      "each card give {X:mult,C:white} x #1# {} mult when card scored",
-      "gain {X:mult,C:white} x #2# {} mult per card scored",
-      "Reset at the end of round",
+      "Evert card give {X:mult,C:white} X#1# {} mult when card scored",
+      "Gain {X:mult,C:white} X#2# {} mult per card scored",
+      "Resets at end of the round",
+      "{s:0.7, C:inactive}Code by CharaMaster"
     }
   },
 
@@ -106,7 +107,8 @@ SMODS.Joker {
       "When first hand is played, put a {C:attention}bounty",
       "on a {C:red}random card{} in hand",
       "When the card with a {C:attention}bounty{} is played,",
-      "gives {C:red}+#1#{} Mult and {C:blue}+#2#{} Chips"
+      "gives {C:red}+#1#{} Mult and {C:blue}+#2#{} Chips",
+      "{s:0.7, C:inactive}Code by AmazinDooD"
     }
   },
   config = {extra = {mult = 4, chips = 50}},
@@ -131,5 +133,71 @@ SMODS.Joker {
         if v.config.center == G.P_CENTERS.m_mvan_bounty then v:set_ability(G.P_CENTERS.c_base, nil, true) end
       end
     end
+  end
+}
+
+
+SMODS.Joker{
+  key = "solar_dice",
+  loc_txt = {
+    name = 'Solar Dice',
+    text = {
+      'Played {C:attention}3s, 5s, 7s, or 9s{}',
+      'give {C:mult}+Mult{} corresponding with their {C:attention}rank{}',
+      "{s:0.7, C:inactive}Code by Valajar"
+    }
+  },
+  rarity = 2,
+  atlas = 'temp',
+  cost = 5,
+  loc_vars = function(self, info_queue, card)
+    return { vars = {} }
+  end,
+  calculate = function(self, card, context)
+    if context.individual and context.cardarea == G.play then
+      local rank = context.other_card:get_id()
+      if rank == 3 or rank == 5 or rank == 7 or rank == 9 then
+        return {
+          mult = rank, -- Use the rank of the card as the multiplier
+          card = context.other_card
+        }
+      end
+    end
+  end
+}
+
+
+
+SMODS.Joker{
+  key = "slingshotdice",
+  loc_txt = {
+    name = 'Slingshot Dice',
+    text = {
+        'Played {C:attention}Aces, 2s, 3s, 4s, or 5s{}',
+        'give {X:mult,C:white}X#1#{} Mult when scored',
+        "{s:0.7, C:inactive}Code by Valajar"
+    }
+  },
+  rarity = 3,
+  atlas = "temp", 
+  cost = 10,
+  unlocked = true,
+  discovered = true,
+  eternal_compat = true,
+  blueprint_compat = true,
+  perishable_compat = true,
+  config = {extra = {x_mult = 1.5}},
+  loc_vars = function(self, info_queue, card)
+    return { vars = { self.config.extra.x_mult } }
+  end,
+  calculate = function(self, card, context)
+      if context.individual and context.cardarea == G.play then
+          if context.other_card:get_id() == 14 or context.other_card:get_id() == 2 or context.other_card:get_id() == 3 or context.other_card:get_id() == 4 or context.other_card:get_id() == 5 then
+            return {
+                x_mult = card.ability.extra.x_mult,
+                card = card
+              }
+          end
+      end
   end
 }
